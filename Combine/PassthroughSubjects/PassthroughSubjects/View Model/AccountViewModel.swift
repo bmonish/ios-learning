cae2cc4cf1602c865fd2893d1efd595d594a597c
Monish B <monishmonib@gmail.com>
2021-08-03 17:01:14 +0530

    Added PSP

--- Combine/PassthroughSubjects/PassthroughSubjects/View Model/AccountViewModel.swift
+++ Combine/PassthroughSubjects/PassthroughSubjects/View Model/AccountViewModel.swift
@@ -1,1 +1,18 @@
 import Foundation
+import Combine
+
+final class AccountViewModel {
+    
+    enum AccountStatus {
+        case active
+        case banned
+    }
+    
+    private let warningLimit = 3
+    
+    let userAccountStatus = CurrentValueSubject<AccountStatus, Never>(.active)
+    let warnings = CurrentValueSubject<Int, Never>(0)
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+}
