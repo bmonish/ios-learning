e6add0f28bb012450d311af16461b043e080e253
Monish B <monishmonib@gmail.com>
2021-09-20 21:57:36 +0530

    CreatingPublishers

--- Combine/AnyPublishers, Just & Never/AnyPublishers, Just & Never/AnyPublishers.playground/Contents.swift
+++ Combine/AnyPublishers, Just & Never/AnyPublishers, Just & Never/AnyPublishers.playground/Contents.swift
@@ -9,3 +9,24 @@ struct Post: Codable {
 }
 
 let postURL = URL(string: "https://jsonplaceholder.typicode.com/posts")
+
+func getPosts() -> AnyPublisher<[Post], Never> {
+    
+    let postsPublisher = URLSession
+        .shared
+        .dataTaskPublisher(for: postURL!)
+        .map { $0.data }
+        .decode(type: [Post].self, decoder: JSONDecoder())
+        .catch { _ in return Just([]) }
+        .eraseToAnyPublisher()
+    
+    return postsPublisher
+}
+
+var subscriptions = Set<AnyCancellable>()
+
+getPosts()
+    .sink { posts in
+        print(posts)
+    }
+    .store(in: &subscriptions)
