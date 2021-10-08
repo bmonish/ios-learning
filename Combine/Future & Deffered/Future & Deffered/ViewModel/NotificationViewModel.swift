73756b9e9d197fb7d8d8b1e7290c266bf96d3f75
Monish B <monishmonib@gmail.com>
2021-10-08 22:09:32 +0530

    Future and Defferred

--- Combine/Future & Deffered/Future & Deffered/ViewModel/NotificationViewModel.swift
+++ Combine/Future & Deffered/Future & Deffered/ViewModel/NotificationViewModel.swift
@@ -4,4 +4,23 @@ import UserNotifications
 
 final class NotificationViewModel {
     
+    func authorize() -> AnyPublisher<Bool, Error> {
+        
+        Deferred {
+            
+            Future { handler in
+                
+                UNUserNotificationCenter
+                    .current()
+                    .requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+                        
+                        if let error = error {
+                            handler(.failure(error))
+                        } else {
+                            handler(.success(granted))
+                        }
+                    }
+            }
+        }.eraseToAnyPublisher()
+    }
 }
