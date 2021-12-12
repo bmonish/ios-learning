167374a721d2ca494f2e22f0e60d6dc4e1ffd185
Monish B <monishmonib@gmail.com>
2021-12-12 14:09:04 +0530

    Added Screenshots for ToolBar

--- ToolBar/README.md
+++ ToolBar/README.md
@@ -1,1 +1,17 @@
 ## UIToolBar
+
+### Evenly Spaced
+<img src="https://user-images.githubusercontent.com/31185862/146894797-8a0be892-972e-4dde-8c5b-8a8c80054642.png" width="300" />
+
+### Left Aligned
+
+<img src="https://user-images.githubusercontent.com/31185862/146895330-c613b2b7-83d9-41d8-be1c-43edcbfa1ddb.png"  width="300" />
+
+### Right Aligned
+<img src="https://user-images.githubusercontent.com/31185862/146895772-cfdeff88-b393-4a21-a76b-f296d095c84d.png" width="300" />
+
+### Left & Right Corners
+<img src="https://user-images.githubusercontent.com/31185862/146895787-111e242c-a13d-4895-a3ee-dc8ff49968a8.png" width="300" />
+
+### With Fixed Space
+<img src="https://user-images.githubusercontent.com/31185862/146896409-e3f4e3eb-092e-4a4c-a568-1729c2ca2b25.png" width="300" />
--- ToolBar/ToolBar/ViewController.swift
+++ ToolBar/ToolBar/ViewController.swift
@@ -12,10 +12,10 @@ class ViewController: UIViewController {
     var label: UILabel!
     
     func setupView(){
-        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label = UILabel()
         label.textAlignment = .center
         label.text = "I'm a test label"
-        label.center = view.center
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         toolBar.barStyle = UIBarStyle(rawValue: 1)!
@@ -24,21 +24,37 @@ class ViewController: UIViewController {
         toolBar.tintColor = UIColor.white
         toolBar.sizeToFit()
         let leftButton = UIBarButtonItem(title: "Left", style: .plain, target: self, action: #selector(tappedButton(_:)))
-        let spaceBetween = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let centerButton = UIBarButtonItem(title: "Center", style: .plain, target: self, action: #selector(tappedButton(_:)))
         let fixedSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        fixedSpace.width = 30
+        fixedSpace.width = 50
         let rightButton = UIBarButtonItem(title: "Right", style: .plain, target: self, action: #selector(tappedButton(_:)))
-        toolBar.setItems([leftButton, spaceBetween, centerButton, fixedSpace, rightButton], animated: true)
-        toolBar.translatesAutoresizingMaskIntoConstraints = false
         
+        // Evenly Spaced
+         toolBar.setItems([leftButton, flexibleSpace, centerButton, flexibleSpace, rightButton], animated: true)
+        
+        // Left Aligned
+         //toolBar.setItems([leftButton, centerButton, rightButton], animated: true)
+        
+        // Right Aligned
+        //toolBar.setItems([flexibleSpace, leftButton, rightButton], animated: true)
+        
+        // Left & Right Corners
+        //toolBar.setItems([leftButton, flexibleSpace, rightButton], animated: true)
+        
+        // Fixed Space
+        //toolBar.setItems([fixedSpace, leftButton, flexibleSpace, rightButton, fixedSpace], animated: true)
+        
+        toolBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(label)
         view.addSubview(toolBar)
         
         NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0),
             toolBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             toolBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            toolBar.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            toolBar.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
         ])
     }
     
