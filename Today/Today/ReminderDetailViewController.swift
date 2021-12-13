53c15bddf2cba3c3429cbee303a6500a3c92217f
Monish B <monishmonib@gmail.com>
2021-12-13 19:06:45 +0530

    Added Today Project

---
+++ Today/Today/ReminderDetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ReminderDetailViewController.swift
+//  Today
+//
+//  Created by monish-pt4649 on 24/11/21.
+//
+
+import UIKit
+
+class ReminderDetailViewController: UITableViewController {
+    typealias ReminderChangeAction = (Reminder) -> Void
+    
+    private var reminder: Reminder?
+    private var tempReminder: Reminder?
+    private var dataSource: UITableViewDataSource?
+    private var reminderChangeAction: ReminderChangeAction?
+    
+    func configure(with reminder: Reminder, changeAction: @escaping ReminderChangeAction) {
+        self.reminder = reminder
+        self.reminderChangeAction = changeAction
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setEditing(false, animated: false)
+        navigationItem.setRightBarButton(editButtonItem, animated: false)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        guard let reminder = reminder else {
+            fatalError("No reminder found for detail view")
+        }
+        if editing {
+            dataSource = ReminderDetailEditDataSource(reminder: reminder) { reminder in
+                self.tempReminder = reminder
+                self.editButtonItem.isEnabled = true
+            }
+            navigationItem.title = NSLocalizedString("Edit Reminder", comment: "edit reminder nav title")
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTrigger))
+            editButtonItem.isEnabled = true
+        } else {
+            if let tempReminder = tempReminder {
+                self.reminder = tempReminder
+                self.tempReminder = nil
+                reminderChangeAction?(tempReminder)
+                dataSource = ReminderDetailViewDataSource(reminder: tempReminder)
+            } else {
+                dataSource = ReminderDetailViewDataSource(reminder: reminder)
+            }
+            navigationItem.title = NSLocalizedString("View Reminder", comment: "view reminder nav title")
+            navigationItem.leftBarButtonItem = nil
+            editButtonItem.isEnabled = true
+        }
+        tableView.dataSource = dataSource
+        tableView.reloadData()
+    }
+    
+    @objc
+    func cancelButtonTrigger() {
+        setEditing(false, animated: true)
+    }
+}
+
+extension ReminderDetailViewController {
+    
+}
+
---
+++ Today/Today/ReminderListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by monish-pt4649 on 23/11/21.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController {
+    
+    private var reminderListDataSource: ReminderListDataSource?
+    
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+           let destination = segue.destination as? ReminderDetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            let rowIndex = indexPath.row
+            guard let reminder = reminderListDataSource?.reminder(at: rowIndex) else {
+                fatalError("Couldn't find data source for reminder list.")
+            }
+            destination.configure(with: reminder) { reminder in
+                self.reminderListDataSource?.update(reminder, at: rowIndex)
+                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
+    }
+}
+
+//extension ReminderListViewController {
+//
+//}
