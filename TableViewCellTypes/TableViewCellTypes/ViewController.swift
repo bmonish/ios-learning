579a1284f5b26da2f21bcdf9a7f2c53fe81e442f
Monish B <monishmonib@gmail.com>
2021-12-14 11:23:27 +0530

    Default Cell Types

---
+++ TableViewCellTypes/TableViewCellTypes/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TableViewCellTypes
+//
+//  Created by monish-pt4649 on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+    
+    @IBOutlet var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    // Default
+    /*
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    */
+    
+    // Value 1
+    /*
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    */
+    
+    // Value 2
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value2, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    // Value 3
+    /*
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+     */
+
+}
+
