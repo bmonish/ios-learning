9356e33caf6b9c5ae24a4b5adacbec90ce3aba5a
Monish B <monishmonib@gmail.com>
2021-12-30 11:38:17 +0530

    Added navVC via Code

---
+++ NavigationControllerWithCode/NavigationControllerWithCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationControllerWithCode
+//
+//  Created by monish-pt4649 on 30/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.title = "Nav Via Code"
+        view.backgroundColor = .red
+    }
+}
+
