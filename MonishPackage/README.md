ff19d232a62da73898bfa6412e8afc255d394cc6
Monish B <monishmonib@gmail.com>
2022-01-11 21:05:04 +0530

    Created a new Function

---
+++ MonishPackage/README.md
@@ -0,0 +1,3 @@
+# MonishPackage
+
+A description of this package.
---
+++ MonishPackage/Sources/MonishPackage/MonishPackage.swift
@@ -0,0 +1,10 @@
+public struct MonishPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func getHello() -> String {
+        return "Hello"
+    }
+}
