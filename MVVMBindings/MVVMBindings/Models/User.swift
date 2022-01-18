75c10b304dbfad421a3762ee46a1e0bf3b7a73fd
Monish B <monishmonib@gmail.com>
2022-01-18 15:15:37 +0530

    Added MVVM Example

---
+++ MVVMBindings/MVVMBindings/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  MVVMBindings
+//
+//  Created by monish-pt4649 on 20/01/22.
+//
+
+import Foundation
---
+++ MVVMBindings/MVVMBindings/ViewModels/UserTableViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserTableViewCellViewModel.swift
+//  MVVMBindings
+//
+//  Created by monish-pt4649 on 20/01/22.
+//
+
+import Foundation
