e186dd052eedd3db5962ecf7a63d6dbee876b0fe
Monish B <monishmonib@gmail.com>
2022-02-14 16:23:52 +0530

    Dispatch Queues

--- Concurrency/DispatchQueues/DispatchQueues/ViewController.swift
+++ Concurrency/DispatchQueues/DispatchQueues/ViewController.swift
@@ -12,8 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getData()
     }
 
+    func calculateSomething() {
+        DispatchQueue.global(qos: .background).async {
+            print("I'm running on a background thread. \(Thread.isMainThread)")
+        }
+    }
+    func getData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users/1") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            guard data != nil else { return }
+            
+            DispatchQueue.main.async {
+                self?.view.backgroundColor = .red
+            }
+        }
+        
+        task.resume()
+    }
 
 }
 
