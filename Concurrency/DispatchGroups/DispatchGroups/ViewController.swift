7f46d904d2475c192ce07fe65294ae401cef3977
Monish B <monishmonib@gmail.com>
2022-02-24 12:27:25 +0530

    Dispatch Group

--- Concurrency/DispatchGroups/DispatchGroups/ViewController.swift
+++ Concurrency/DispatchGroups/DispatchGroups/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let waitTimes: [TimeInterval] = [
+        1,
+        3,
+        5,
+        8,
+        2,
+        7
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
     }
-
-
+    
+    func getData() {
+        let group = DispatchGroup()
+        
+        for number in waitTimes {
+            
+            group.enter()
+            print("Entering group with number: \(number)")
+            
+            DispatchQueue.global().asyncAfter(deadline: .now() + number, execute: {
+                group.leave()
+                print("Leaving group for \(number)")
+            })
+            
+        }
+        
+        group.notify(queue: .main, execute: {
+            print("Done with all operations")
+            self.view.backgroundColor = .blue
+        })
+    }
 }
 
